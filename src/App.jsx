ccf816c6194df56059e9e441a220bc084f437765
vickyruud <aprajit.k@gmail.com>
2022-05-01 14:49:39 -0400

    added login with google

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import NavBar from './components/NavBar';
 import SignIn from './modals/SignIn';
 import SignUp from './modals/SignUp';
 import { Modal } from '@mui/material';
+import GoogleLogin from 'react-google-login';
+
+
+
 
 export default function App() {
 
@@ -43,7 +47,7 @@ export default function App() {
 
   useEffect(() => {
     const loggedInUser = localStorage.getItem('user');
-    if (loggedInUser != undefined) {
+    if (loggedInUser !== undefined) {
       let foundUser = JSON.parse(loggedInUser)
       setUser(foundUser);
     } else {
@@ -51,12 +55,38 @@ export default function App() {
     }
   }, [])
 
+
+  const responseSuccessGoogle = (response) => {
+    axios({
+      method: "POST",
+      url: 'http://localhost:5000/api/googlelogin',
+      data: {tokenId: response.tokenId}
+    }).then(resp => {
+      console.log(resp);
+      localStorage.setItem("token", resp.data.token) 
+      localStorage.setItem("user", JSON.stringify(resp.data.message)) 
+      setUser(resp.data.message);
+      
+    })
+  }
+
+  const responseErrorGoogle = (response) => {
+    
+  }
+
   
   return (
     <div>
 
       {!user && <NavBar handleSignInOpen={setSignInOpen} setOpenModal={setSignUpOpen} />}
       {user && <NavBar user={user} logout={logout} />}
+        <GoogleLogin
+          clientId="314086644675-a82vonng714fusuuotog2780t6plapo9.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
 
       <Modal open={signInOpen}>
       <SignIn completeSignUp={completeSignUp} setCompleteSignUp={setCompleteSignUp} setUser={setUser} setOpenModal={setSignInOpen} />
